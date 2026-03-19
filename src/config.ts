6a731b26c1e767a9d898afd059c14dfb0a3c9166
Daniel Shanahan <dshanahan1@students.ccu.edu>
2026-03-18 22:11:58 -0400

    fix: apply solution for issue #91

---
+++ src/config.ts
@@ -0,0 +1,6 @@
+export const SERVICE_NAME = 'my-scraper';
+export const PRICE_USDC = 0.005;
+export const DESCRIPTION = 'What it does';
+export const SERP_TRACKER_PRICE_USDC = 0.005;
+export const GOOGLE_MAPS_LEAD_GENERATOR_PRICE_USDC = 0.005;
+export const GOOGLE_REVIEWS_PRICE_USDC = 0.005;
